fb8a6d36b3ea2aebe6a3eb7b2aba189fc6525124
Shunting Zhang <shunting@fb.com>
2017-08-13 00:06:27 -0700

    Use image view to show picture of guitar

--- app/src/main/java/com/andworld/explorer/MainActivity.java
+++ app/src/main/java/com/andworld/explorer/MainActivity.java
@@ -3,14 +3,20 @@ package com.andworld.explorer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    /*
     TextView tv = new TextView(this);
     tv.setText("Hello, this is andworld explorer!");
     setContentView(tv);
+     */
+    ImageView iv = new ImageView(this);
+    iv.setImageResource(R.drawable.guitar);
+    setContentView(iv);
   }
 }
